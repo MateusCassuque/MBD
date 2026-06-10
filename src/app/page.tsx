86d93a44980e904aebc9f159373af2e5dd025cbe
Mateus Cassuque <100739934+MateusCassuque@users.noreply.github.com>
2026-06-10 11:10:53 +0100

    Foi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@
 import { Lang, translations } from "@/hooks/translations"
 import { useState } from "react"
 import Navbar from "./components/landing/Navbar"
-import Hero from "./components/landing/Hero"
 import About from "./components/landing/About"
 import Businesses from "./components/landing/Businesses"
 import Footer from "./components/landing/Footer"
 import Services from "./components/landing/Services"
 import Contact from "./components/landing/Contact"
 import ProductsPage from "./components/ProductsPage"
+import Hero from "./components/landing/Hero"
 
 type Page = "home" | "products"
 
@@ -19,7 +19,7 @@ function Divider() {
     )
 }
 
-export default function App() {
+export default function HomePage() {
     const [lang, setLang] = useState<Lang>("pt")
     const [page, setPage] = useState<Page>("home")
     const t = translations[lang]
